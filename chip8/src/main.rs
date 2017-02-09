e3ee0f560b4ff2c4c1126ae7b3f298a9fe675e29
Matti Jokitulppo <melonmanchan@gmail.com>
2017-02-09 22:56:55 +0200

    Declare some variables

--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -1,3 +1,29 @@
+
 fn main() {
+    let mut current_opcode: u16;
+
+    // Memory of the emulator
+    let memory: [u8; 4096];
+
+    // CPU registers (From 0 to E)
+    let V: [u8; 16];
+
+    let mut index_register: u16;
+    let mut program_counter: u16;
+
+    // 64 x 32 screen of black-and-white pixels
+    // Either on or off.
+    let graphics: [u8; 64 * 32];
+
+    // Timers
+    let mut delay_timer: u8;
+    let mut sound_timer: u8;
+
+    let stack: [u16; 16];
+    let mut stack_pointer: u16;
+
+    // Whichever keys are pressed
+    let keys: [u8; 16];
+
     println!("Hello, world!");
 }
