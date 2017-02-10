d9d33e9320acbfb9d10359365d1eff7c5a8332a5
Matti Jokitulppo <melonmanchan@gmail.com>
2017-02-10 15:23:49 +0200

    Refactor chip8 functions into struct

--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -1,42 +1,3 @@
-fn setup_graphics(chip8: &mut Chip8) {
-
-}
-
-fn setup_input(chip8: &mut Chip8) {
-
-}
-
-fn load_game(chip8: &mut Chip8) {
-
-}
-
-fn emulate_cycle(chip8: &mut Chip8) {
-    let pc = chip8.program_counter as usize;
-
-    let current_opcode: u16 = (chip8.memory[pc] as u16) << 8 | (chip8.memory[pc + 1] as u16);
-
-
-    if chip8.delay_timer > 0 {
-        chip8.delay_timer = chip8.delay_timer - 1;
-    }
-
-    if chip8.sound_timer > 0 {
-        if chip8.sound_timer == 1 {
-            println!("BEEPS")
-        }
-
-        chip8.sound_timer = chip8.sound_timer - 1;
-    }
-}
-
-fn draw_graphics(chip8: &mut Chip8) {
-
-}
-
-fn set_key_state(chip8: &mut Chip8) {
-
-}
-
 struct Chip8 {
     current_opcode: u16,
     // Memory of the emulator
@@ -63,6 +24,47 @@ struct Chip8 {
     keys: [u8; 16],
 }
 
+impl Chip8 {
+    fn setup_graphics(&self) {
+
+    }
+
+    fn setup_input(&self) {
+
+    }
+
+    fn load_game(&self) {
+
+    }
+
+    fn emulate_cycle(&mut self) {
+        let pc = self.program_counter as usize;
+
+        let current_opcode: u16 = (self.memory[pc] as u16) << 8 | (self.memory[pc + 1] as u16);
+
+        if self.delay_timer > 0 {
+            self.delay_timer = self.delay_timer - 1;
+        }
+
+        if self.sound_timer > 0 {
+            if self.sound_timer == 1 {
+                println!("BEEPS")
+            }
+
+            self.sound_timer = self.sound_timer - 1;
+        }
+    }
+
+    fn draw_graphics(&self) {
+
+    }
+
+    fn set_key_state(&self) {
+
+    }
+}
+
+
 fn main() {
     let mut chip8: Chip8 = Chip8 {
         current_opcode:  0,
@@ -85,16 +87,18 @@ fn main() {
         keys: [0; 16]
     };
 
-    setup_graphics(&mut chip8);
-    setup_input(&mut chip8);
+    chip8.setup_graphics();
 
-    load_game(&mut chip8);
+    chip8.setup_input();
+
+
+    chip8.load_game();
 
     loop {
-        emulate_cycle(&mut chip8);
+        chip8.emulate_cycle();
 
-        draw_graphics(&mut chip8);
+        chip8.draw_graphics();
 
-        set_key_state(&mut chip8);
+        chip8.set_key_state();
     }
 }
